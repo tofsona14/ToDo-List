3dc9cbc274d99ba6a4add334c2533bfcf4f3b720
tofsona14 <topuria2074@gmail.com>
2022-12-29 18:12:48 +0400

    Make component addPlan

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
 import React from 'react'
 import './App.css';
 import './fonts/Ohio Bro - Free.otf'
+import {AddPlan} from './AddPlan/addPlan';
 
 function App() {
-
-   const date = new Date();
-   const today = `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`
-  return (
-    <div className='DateDiv'>
-      <div className='Div--Date'><h1 className='Date'>Date:{today}</h1></div>
-      <div className="InfoDiv--1">
-        <label htmlFor='EnterText' className='g' > Type Daily Plan </label>
-        <input className='inputDailyPlan' type="text" id='EnterText' maxLength={50} placeholder="you can type only 50 word"/>
-      </div>
-      <br></br>
-      <div className='InfoDiv--2'>
-        <label htmlFor='EnterDate'>
-            Enter Clock Of Current Daily plan
-        </label>
-        <input className='inputOfTime' type="number" min={0} max={24} step={1} placeholder='Enter Time ex:8 (24 Hour standard)'/>
-      </div>
+  return(
+    <div>
+      {<AddPlan />}
     </div>
-  );
+  )
 }
 
 export default App;
