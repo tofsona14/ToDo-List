70969a0ac62a0fe22ba6e6244cab3593b2c537e6
tofsona14 <topuria2074@gmail.com>
2022-12-29 16:24:00 +0400

    add Daily Plan Time

--- src/App.js
+++ src/App.js
@@ -8,10 +8,18 @@ function App() {
    const today = `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`
   return (
     <div className='DateDiv'>
-       <div className='Div--Date'><h1 className='Date'>Date:{today}</h1></div>
-      <label htmlFor='EnterText' className='dailyPlanText' > Type Daily Plan </label>
-      <input className='inputDailyPlan' type="text" id='EnterText' maxLength={50} placeholder="you can type only 50 word"/>
-        
+      <div className='Div--Date'><h1 className='Date'>Date:{today}</h1></div>
+      <div className="InfoDiv--1">
+        <label htmlFor='EnterText' className='g' > Type Daily Plan </label>
+        <input className='inputDailyPlan' type="text" id='EnterText' maxLength={50} placeholder="you can type only 50 word"/>
+      </div>
+      <br></br>
+      <div className='InfoDiv--2'>
+        <label htmlFor='EnterDate'>
+            Enter Clock Of Current Daily plan
+        </label>
+        <input className='inputOfTime' type="number" min={0} max={24} step={1} placeholder='Enter Time ex:8 (24 Hour standard)'/>
+      </div>
     </div>
   );
 }
