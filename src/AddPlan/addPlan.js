0e46fcdc416e2a5e1e1159c8661cebc93939e15d
MichaLL27 <lazarashvili2704@gmail.com>
2022-12-30 20:58:39 +0400

    add buttons

--- src/AddPlan/addPlan.js
+++ src/AddPlan/addPlan.js
@@ -1,35 +1,51 @@
-import React from 'react'
-import './addPlan.css';
-import '../fonts/Ohio Bro - Free.otf'
+import React from "react";
+import "./addPlan.css";
+import "../fonts/Ohio Bro - Free.otf";
 
-
- export const AddPlan = () => {
-    const date = new Date();
-    const today = `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`
-    const submit = (event) => {
-        event.preventDefault();
-        console.log(event)
-    }
+export const AddPlan = () => {
+  const date = new Date();
+  const today = `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`;
+  const submit = (event) => {
+    event.preventDefault();
+    console.log(event);
+  };
   return (
-    <div className='DateDiv'>
-      <div className='Div--Date'><h1 className='Date'>Date:{today}</h1></div>
+    <div className="DateDiv">
+      <div className="Div--Date">
+        <h1 className="Date">Date:{today}</h1>
+      </div>
       <div className="InfoDiv--1">
-        <label htmlFor='EnterText' className='g' > Type Daily Plan </label>
-        <input className='inputDailyPlan' type="text" id='EnterText' maxLength={50} placeholder="you can type only 50 word"/>
-      </div>
-      <br></br>
-      <div className='InfoDiv--2'>
-        <label htmlFor='EnterDate'>
-            Enter Clock Of Current Daily plan
+        <label htmlFor="EnterText" className="g">
+          {" "}
+          Type Daily Plan{" "}
         </label>
-        <input className='inputOfTime' id='EnterDate' type="number" min={0} max={24} step={1} placeholder='Enter Time ex:8 (24 Hour standard)'/>
+        <input
+          className="inputDailyPlan"
+          type="text"
+          id="EnterText"
+          maxLength={50}
+          placeholder="you can type only 50 word"
+        />
+      </div>
+      <br></br>
+      <div className="InfoDiv--2">
+        <label htmlFor="EnterDate">Enter Clock Of Current Daily plan</label>
+        <input
+          className="inputOfTime"
+          id="EnterDate"
+          type="number"
+          min={0}
+          max={24}
+          step={1}
+          placeholder="Enter Time ex:8 (24 Hour standard)"
+        />
       </div>
       <br></br>
       <br></br>
-        <div className='ButtonsOfAdd'>
-        <button />
-        </div>
+      <div className="ButtonsOfAdd">
+        <button className="firstButton">Add</button>
+        <button className="secondButton">Cancel</button>
+      </div>
     </div>
-
   );
-}
+};
