db105f5a1e156974ac45f0e17f4a8d143bfe912e
tofsona14 <topuria2074@gmail.com>
2022-12-31 22:42:19 +0400

    add 00:00 syntax

--- src/AddPlan/todoObject/objectPlan.js
+++ src/AddPlan/todoObject/objectPlan.js
@@ -9,7 +9,7 @@ export const ObjectPlan = () => {
       ob: 0,
       id: 1,
       plane: "Wake Up early",
-      time: "08:00",
+      time: 8,
     },
   ];
   const [fullInfo, setFullInfo] = useState(object)
@@ -18,17 +18,22 @@ export const ObjectPlan = () => {
       return [...prev, arg]
     })
   }
-
-  
+  const Statement = (x) => {
+    if (Number(x.time) < 10) {
+      return `0${x.time}:00`
+    }else {
+      return `${x.time}:00`
+    }
+  }
 
   return (
     <div>
       <div className="ourTodo">
-        <h1 className="TodoList">Your Todo List!;</h1>
+        <h1 className="TodoList">Your Todo List</h1>
       </div>
       {fullInfo.map((x) => 
 
-        <OutputTodo key={fullInfo.indexOf(x) + 1} id={fullInfo.indexOf(x) + 1} plane={x.plane} time={x.time} />
+        <OutputTodo key={fullInfo.indexOf(x) + 1} id={fullInfo.indexOf(x) + 1} plane={x.plane} time={Statement(x)} />
         
       )}
       <AddPlan onSave = {AddInfo}/>
