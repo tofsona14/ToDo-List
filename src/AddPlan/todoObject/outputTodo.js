19b6a7f640d8dd48509efc113cdaa550e4c3165a
tofsona14 <topuria2074@gmail.com>
2023-01-02 13:28:49 +0400

    add checkbox and styling

--- src/AddPlan/todoObject/outputTodo.js
+++ src/AddPlan/todoObject/outputTodo.js
@@ -4,13 +4,15 @@ import "./outputTodo.css";
 export const OutputTodo = (props) => {
   return (
     <div className="outputTexts">
-
       <div className="both">
+        <div className="bothDiv">
         <h1>
           <span>{props.id} . </span>
           {props.plane}
         </h1>
-        <h1>{props.time}</h1>
+        </div>
+        <h1 className="bothTime">{props.time}</h1>
+        <input className="bothCheckbox" type='checkbox'></input>
       </div>
     </div>
   );
