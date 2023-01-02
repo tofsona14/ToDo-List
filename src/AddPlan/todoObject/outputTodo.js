c81fa60704d5cc654acb1d66d1d03c971f810ccc
tofsona14 <topuria2074@gmail.com>
2023-01-02 21:06:45 +0400

    add some static info and identificator of checkboxes <3

--- src/AddPlan/todoObject/outputTodo.js
+++ src/AddPlan/todoObject/outputTodo.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React,{useState} from "react";
 import "./outputTodo.css";
 
 export const OutputTodo = (props) => {
+  const [done, setDone] = useState(true)
+  const [notDone, setNotDone] = useState(true)
+  const doneCheckbox = () => {
+    if(done == false) { 
+      setDone(true)
+      console.log(done)
+    }else {
+      setDone(false)
+      console.log(done)
+    }
+  }
+  const notDoneCheckbox = (arg) => {
+    if(notDone == false) {
+      setNotDone(true)
+      console.log(arg.target.id)
+      console.log(`button 2 is in ${notDone}`)
+    }else {
+      setNotDone(false) 
+      console.log(`button 2 is in ${notDone}`)
+
+      }
+    }
+  
+  
   return (
     <div className="outputTexts">
       <div className="both">
@@ -12,8 +36,9 @@ export const OutputTodo = (props) => {
         </h1>
         </div>
         <h1 className="bothTime">{props.time}</h1>
-        <input className="bothCheckbox" type='checkbox'></input>
+        <input id={props.id} onClick={notDoneCheckbox} className="bothCheckbox" type='checkbox'></input>
+        <input onClick={doneCheckbox} className="bothCheckbox" type='checkbox'></input>
       </div>
     </div>
   );
-};
+  }
