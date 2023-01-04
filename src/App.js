fb893df26bb585d680934497544db57bda4b1204
tofsona14 <topuria2074@gmail.com>
2023-01-04 23:11:11 +0400

    add Main and some css

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from 'react'
 import './App.css';
 import './fonts/Ohio Bro - Free.otf'
 import { ObjectPlan } from './AddPlan/todoObject/objectPlan';
+import Main from './MainPage/main'
 function App() {
   return(
     <div>
-      {<ObjectPlan />}
+      <Main />      
     </div>
   )
 }
---
+++ src/MainPage/main.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './main.css'
+
+
+const Main = () => {
+    return(
+        <div className='main'>
+            <div className='main-child'>
+                <div className='main-button'>
+                    <div>
+                        <button>Daily Plan</button>
+                    </div>
+                    <br></br>
+                    <div>
+                        <button>Add Plan</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Main
